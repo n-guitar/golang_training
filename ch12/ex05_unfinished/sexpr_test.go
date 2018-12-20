c599fded201e19595e959a00ca229a5389e5e6ab
n-guitar <naoto.takita@gmail.com>
2018-12-20 21:55:16 +0900

    ch12

---
+++ ch12/ex05_unfinished/sexpr_test.go
@@ -0,0 +1,69 @@
+package sexpr
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+// Test verifies that encoding and decoding a complex data value
+// produces an equal result.
+//
+// The test does not make direct assertions about the encoded output
+// because the output depends on map iteration order, which is
+// nondeterministic.  The output of the t.Log statements can be
+// inspected by running the test with the -v flag:
+//
+// 	$ go test -v gopl.io/ch12/sexpr
+//
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor",
+			"Best AdaptedScreenplay",
+			"Best Director",
+			"Best Picture",
+		},
+	}
+
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	data, err = MarshalIndent(strangelove)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var movie Movie
+	err = json.Unmarshal(data, &movie)
+	if err != nil {
+		t.Fatalf("Json unmarshal err %v", err)
+	}
+	t.Logf("json.Unmarshal() = \n%+v\n", movie)
+
+	t.Logf("MarshalIdent() = %s\n", data)
+	fmt.Printf("%s\n", data)
+
+}
